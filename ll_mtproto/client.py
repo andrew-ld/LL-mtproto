f311c1330f09769f655ed6b04618f952c9c6a5a4
andrew (from workstation) <43882924+andrew-ld@users.noreply.github.com>
2022-04-01 22:01:33 +0200

    cap salt to 30 minutes

--- ll_mtproto/client.py
+++ ll_mtproto/client.py
@@ -294,7 +294,7 @@ class Client:
         if valid_salt := next((salt for salt in body.salts if salt.valid_since <= body.now), False):
             self._auth_key.server_salt = valid_salt.salt
 
-            salt_expire = max(valid_salt.valid_until - body.now + 1, 1)
+            salt_expire = min(max(valid_salt.valid_until - body.now + 1, 1), 1801)
 
             self._pending_future_salt = self._loop.call_later(
                 salt_expire,
