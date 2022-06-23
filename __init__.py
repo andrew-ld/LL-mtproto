111da1840a319cfa431f01e5168b17c5c4db9369
andrew (from workstation) <43882924+andrew-ld@users.noreply.github.com>
2022-06-23 19:12:52 +0200

    init info

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .ll_mtproto import Client, TelegramSchema, AuthKey, TelegramDatacenter, RpcError
+from .ll_mtproto import Client, TelegramSchema, AuthKey, TelegramDatacenter, RpcError, ConnectionInfo
 
-__all__ = ("Client", "TelegramDatacenter", "TelegramSchema", "AuthKey", "RpcError")
+__all__ = ("Client", "TelegramDatacenter", "TelegramSchema", "AuthKey", "RpcError", "ConnectionInfo")
