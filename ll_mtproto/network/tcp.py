09838eb1292bef57645a9720260521705590d17a
andrew (from workstation) <43882924+andrew-ld@users.noreply.github.com>
2022-07-16 21:54:46 +0200

    prevent connection change while write payload

--- ll_mtproto/network/tcp.py
+++ ll_mtproto/network/tcp.py
@@ -15,6 +15,34 @@ class AbridgedTCP:
     _write_lock: asyncio.Lock
     _read_buffer: bytearray
 
+    @staticmethod
+    async def _write_abridged_packet(data: bytes, writer: asyncio.StreamWriter):
+        packet_data_length = len(data) >> 2
+
+        if packet_data_length < 0x7F:
+            writer.write(packet_data_length.to_bytes(1, "little"))
+
+        elif packet_data_length <= 0x7FFFFF:
+            writer.write(b"\x7f")
+            writer.write(packet_data_length.to_bytes(3, "little"))
+
+        else:
+            raise OverflowError("Packet data is too long")
+
+        writer.write(data)
+
+    @staticmethod
+    async def _read_abridged_packet(reader: asyncio.StreamReader) -> bytes:
+        packet_data_length = ord(await reader.readexactly(1))
+
+        if packet_data_length > 0x7F:
+            raise NotImplementedError(f"Wrong packet data length {packet_data_length:d}")
+
+        if packet_data_length == 0x7F:
+            packet_data_length = int.from_bytes(await reader.readexactly(3), "little", signed=False)
+
+        return await reader.readexactly(packet_data_length * 4)
+
     def __init__(self, host: str, port: int):
         self._loop = asyncio.get_event_loop()
         self._host = host
@@ -36,48 +64,21 @@ class AbridgedTCP:
 
             return reader, writer
 
-    async def _write_abridged_packet(self, data: bytes):
-        reader, writer = await self._reconnect_if_needed()
-
-        packet_data_length = len(data) >> 2
-
-        if packet_data_length < 0x7F:
-            writer.write(packet_data_length.to_bytes(1, "little"))
-
-        elif packet_data_length <= 0x7FFFFF:
-            writer.write(b"\x7f")
-            writer.write(packet_data_length.to_bytes(3, "little"))
-
-        else:
-            raise OverflowError("Packet data is too long")
-
-        writer.write(data)
-
-    async def _read_abridged_packet(self) -> bytes:
-        reader, writer = await self._reconnect_if_needed()
-
-        packet_data_length = ord(await reader.readexactly(1))
-
-        if packet_data_length > 0x7F:
-            raise NotImplementedError(f"Wrong packet data length {packet_data_length:d}")
-
-        if packet_data_length == 0x7F:
-            packet_data_length = int.from_bytes(await reader.readexactly(3), "little", signed=False)
-
-        return await reader.readexactly(packet_data_length * 4)
-
     async def read(self) -> bytes:
         if self._read_buffer:
             result = bytes(self._read_buffer)
             self._read_buffer.clear()
         else:
-            result = await self._read_abridged_packet()
+            reader, _ = await self._reconnect_if_needed()
+            result = await self._read_abridged_packet(reader)
 
         return result
 
     async def readn(self, n: int) -> bytes:
+        reader, _ = await self._reconnect_if_needed()
+
         while len(self._read_buffer) < n:
-            self._read_buffer += await self._read_abridged_packet()
+            self._read_buffer += await self._read_abridged_packet(reader)
 
         result = self._read_buffer[:n]
         del self._read_buffer[:n]
@@ -86,12 +87,14 @@ class AbridgedTCP:
     async def write(self, data: bytes):
         data = bytearray(data)
 
+        _, writer = await self._reconnect_if_needed()
+
         async with self._write_lock:
             while (data_len := len(data)) > 0:
                 chunk_len = min(data_len, 0x7FFFFF)
                 chunk_mem = data[:chunk_len]
                 del data[:chunk_len]
-                await self._write_abridged_packet(chunk_mem)
+                await self._write_abridged_packet(chunk_mem, writer)
 
     def stop(self):
         if writer := self._writer:
