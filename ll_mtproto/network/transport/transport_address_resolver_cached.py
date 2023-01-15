e4f2bfc4b495c5153db59303d966f81fc8e4e002
andrew (from workstation) <43882924+andrew-ld@users.noreply.github.com>
2023-01-15 22:20:45 +0100

    support multiple addresses on cached resolver

--- ll_mtproto/network/transport/transport_address_resolver_cached.py
+++ ll_mtproto/network/transport/transport_address_resolver_cached.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 from ...network import DatacenterInfo
 from ...network.transport import TransportAddressResolverBase
@@ -10,32 +11,40 @@ __all__ = ("CachedTransportAddressResolver",)
 class CachedTransportAddressResolver(TransportAddressResolverBase):
     __slots__ = ("_cached_resolved",)
 
-    _cached_resolved: dict[DatacenterInfo, tuple[str, int]]
+    _cached_resolved: dict[DatacenterInfo, list[tuple[str, int]]]
 
     def __init__(self):
         self._cached_resolved = dict()
 
     def on_new_address(self, datacenter_info: DatacenterInfo, direct_address: str, direct_port: int):
-        self._cached_resolved[datacenter_info] = (direct_address, direct_port)
+        self._cached_resolved.setdefault(datacenter_info, []).append((direct_address, direct_port))
 
-    def get_cache_copy(self) -> dict[DatacenterInfo, tuple[str, int]]:
+    def get_cache_copy(self) -> dict[DatacenterInfo, list[tuple[str, int]]]:
         return copy.deepcopy(self._cached_resolved)
 
-    def apply_telegram_config(self, datacenters: list[DatacenterInfo], config: Structure):
+    def apply_telegram_config(self, datacenters: list[DatacenterInfo], config: Structure, allow_ipv6: bool = False):
         if config.constructor_name != "config":
             raise TypeError(f"Expected: config, Found: {config!r}")
 
-        for dc_option in filter(lambda dc_option: not dc_option.ipv6, config.dc_options):
-            found_dc = next(
+        supported_dc_options = config.dc_options
+
+        if not allow_ipv6:
+            supported_dc_options = filter(lambda dc_option: not dc_option.ipv6, supported_dc_options)
+
+        for datacenter in datacenters:
+            self._cached_resolved.pop(datacenter, None)
+
+        for dc_option in supported_dc_options:
+            found_datacenter = next(
                 datacenter
                 for datacenter in datacenters
                 if datacenter.datacenter_id == dc_option.id and datacenter.is_media == bool(dc_option.media_only)
             )
 
-            self.on_new_address(found_dc, dc_option.ip_address, dc_option.port)
+            self.on_new_address(found_datacenter, dc_option.ip_address, dc_option.port)
 
     async def get_address(self, datacenter_info: DatacenterInfo) -> tuple[str, int]:
         if cached_address := self._cached_resolved.get(datacenter_info, None):
-            return cached_address
+            return random.choice(cached_address)
         else:
             return datacenter_info.default_direct_address, datacenter_info.default_direct_port
