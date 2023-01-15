22c3245556a25a44b716ff3738f0ba6b5576f6d1
andrew (from workstation) <43882924+andrew-ld@users.noreply.github.com>
2023-01-15 22:18:45 +0100

    support multiple addresses on cached resolver

--- ll_mtproto/network/transport/transport_address_resolver_cached.py
+++ ll_mtproto/network/transport/transport_address_resolver_cached.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 from ...network import DatacenterInfo
 from ...network.transport import TransportAddressResolverBase
@@ -10,15 +11,15 @@ __all__ = ("CachedTransportAddressResolver",)
 class CachedTransportAddressResolver(TransportAddressResolverBase):
     __slots__ = ("_cached_resolved",)
 
-    _cached_resolved: dict[DatacenterInfo, tuple[str, int]]
+    _cached_resolved: dict[DatacenterInfo, list[tuple[str, int]]]
 
     def __init__(self):
         self._cached_resolved = dict()
 
     def on_new_address(self, datacenter_info: DatacenterInfo, direct_address: str, direct_port: int):
-        self._cached_resolved[datacenter_info] = (direct_address, direct_port)
+        self._cached_resolved.setdefault(datacenter_info, []).append((direct_address, direct_port))
 
-    def get_cache_copy(self) -> dict[DatacenterInfo, tuple[str, int]]:
+    def get_cache_copy(self) -> dict[DatacenterInfo, list[tuple[str, int]]]:
         return copy.deepcopy(self._cached_resolved)
 
     def apply_telegram_config(self, datacenters: list[DatacenterInfo], config: Structure, allow_ipv6: bool = False):
@@ -29,6 +30,9 @@ class CachedTransportAddressResolver(TransportAddressResolverBase):
 
         if not allow_ipv6:
             supported_dc_options = filter(lambda dc_option: not dc_option.ipv6, supported_dc_options)
+
+        for datacenter in datacenters:
+            self._cached_resolved.pop(datacenter, None)
 
         for dc_option in supported_dc_options:
             found_datacenter = next(
@@ -41,6 +45,6 @@ class CachedTransportAddressResolver(TransportAddressResolverBase):
 
     async def get_address(self, datacenter_info: DatacenterInfo) -> tuple[str, int]:
         if cached_address := self._cached_resolved.get(datacenter_info, None):
-            return cached_address
+            return random.choice(cached_address)
         else:
             return datacenter_info.default_direct_address, datacenter_info.default_direct_port
