a46cccd7f0f86a30fb12583b57f165124c95af75
andrew (from workstation) <43882924+andrew-ld@users.noreply.github.com>
2023-01-21 10:50:02 +0100

    add copyright notice

--- ll_mtproto/tl/__init__.py
+++ ll_mtproto/tl/__init__.py
@@ -1,6 +1,23 @@
+# Copyright (C) 2017-2018 (nikat) https://github.com/nikat/mtproto2json
+# Copyright (C) 2020-2023 (andrew) https://github.com/andrew-ld/LL-mtproto
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from .byteutils import xor, base64encode, base64decode, sha1, sha256, to_bytes, pack_binary_string, \
-    unpack_binary_string_header, ByteReaderApply, unpack_binary_string_stream, unpack_long_binary_string_stream,\
-    unpack_binary_string, pack_long_binary_string, long_hex, short_hex, short_hex_int, reader_is_empty, reader_discard,\
+    unpack_binary_string_header, ByteReaderApply, unpack_binary_string_stream, unpack_long_binary_string_stream, \
+    unpack_binary_string, pack_long_binary_string, long_hex, short_hex, short_hex_int, reader_is_empty, reader_discard, \
     GzipStreamReader, to_reader, to_composed_reader, SyncByteReaderApply, pack_long_binary_string_padded
 
 from .tl import Schema, Value, Structure, Parameter, Constructor
