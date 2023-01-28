d2750c615daf39907e6c208006c73ad9c376fd7b
andrew (from workstation) <43882924+andrew-ld@users.noreply.github.com>
2023-01-28 10:43:36 +0100

    add lru cache for is_safe_dh_prime

--- ll_mtproto/math/primes.py
+++ ll_mtproto/math/primes.py
@@ -15,6 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+import functools
+
 import cryptg
 
 __all__ = ("factorize", "is_safe_dh_prime")
@@ -24,6 +26,7 @@ def factorize(pq: int) -> tuple[int, int]:
     return cryptg.factorize_pq_pair(pq)
 
 
+@functools.lru_cache
 def is_safe_dh_prime(g: int, n: int) -> bool:
     match g:
         case 2:
