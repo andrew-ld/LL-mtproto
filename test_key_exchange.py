81c44ec7fd846006b9d8f5622d3622d1aad0c4ef
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2023-12-28 23:15:23 +0100

    add more test

---
+++ test_key_exchange.py
@@ -0,0 +1,52 @@
+import asyncio
+import concurrent.futures
+import logging
+import typing
+
+from ll_mtproto import TelegramDatacenter
+from ll_mtproto.crypto.auth_key import DhGenKey
+from ll_mtproto.crypto.providers.crypto_provider_cryptg import CryptoProviderCryptg
+from ll_mtproto.network.dh.mtproto_key_creator_dispatcher import initialize_key_creator_dispatcher
+from ll_mtproto.network.dispatcher import dispatch_event
+from ll_mtproto.network.mtproto import MTProto
+from ll_mtproto.network.transport.transport_address_resolver_cached import CachedTransportAddressResolver
+from ll_mtproto.network.transport.transport_codec_abridged import TransportCodecAbridgedFactory
+from ll_mtproto.network.transport.transport_link_tcp import TransportLinkTcpFactory
+
+
+async def main() -> typing.NoReturn:
+    blocking_executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+    datacenter = TelegramDatacenter.VESTA
+    crypto_provider = CryptoProviderCryptg()
+
+    async def in_thread(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+        return await asyncio.get_running_loop().run_in_executor(blocking_executor, *args, **kwargs)
+
+    mtproto = MTProto(
+        datacenter,
+        TransportLinkTcpFactory(TransportCodecAbridgedFactory(), CachedTransportAddressResolver()),
+        in_thread,
+        crypto_provider,
+    )
+
+    while True:
+        result: asyncio.Future[DhGenKey] = asyncio.get_running_loop().create_future()
+
+        dispatcher = await initialize_key_creator_dispatcher(
+            result,
+            False,
+            mtproto,
+            in_thread,
+            TelegramDatacenter.VESTA,
+            crypto_provider
+        )
+
+        while not result.done():
+            await dispatch_event(dispatcher, mtproto, None)
+
+        print("key generated", result.result().auth_key_id)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(level=logging.DEBUG)
+    asyncio.run(main())
---
+++ test_performance.py
@@ -0,0 +1,96 @@
+import asyncio
+import concurrent.futures
+import logging
+import time
+import traceback
+
+from ll_mtproto import *
+from ll_mtproto.network.transport.transport_codec_abridged import TransportCodecAbridgedFactory
+from ll_mtproto.network.transport.transport_codec_base import TransportCodecBase
+from ll_mtproto.network.transport.transport_codec_factory import TransportCodecFactory
+
+
+class TransportCodecPerformanceTrack(TransportCodecBase):
+    _parent_codec: TransportCodecBase
+    _first_write_time: int | None
+    _first_read: bool
+
+    def __init__(self, parent_codec: TransportCodecBase):
+        self._parent_codec = parent_codec
+        self._first_write_time = None
+        self._first_read = True
+
+    async def write_packet(self, writer: asyncio.StreamWriter, data: bytes) -> None:
+        try:
+            return await self._parent_codec.write_packet(writer, data)
+        finally:
+            if self._first_write_time is None:
+                self._first_write_time = time.time()
+
+    async def read_packet(self, reader: asyncio.StreamReader) -> bytes:
+        try:
+            return await self._parent_codec.read_packet(reader)
+        finally:
+            if self._first_read:
+                self._first_read = False
+                print("difference: ", time.time() - self._first_write_time)
+
+
+class TransportCodecPerformanceTrackFactory(TransportCodecFactory):
+    _parent_factory: TransportCodecFactory
+
+    def __init__(self, parent_factory: TransportCodecFactory):
+        self._parent_factory = parent_factory
+
+    def new_codec(self) -> TransportCodecBase:
+        return TransportCodecPerformanceTrack(self._parent_factory.new_codec())
+
+
+async def test():
+    connection_info = ConnectionInfo(
+        api_id=6,
+        device_model="test",
+        system_version="test",
+        app_version="1.0",
+        lang_code="de",
+        system_lang_code="de",
+        lang_pack=""
+    )
+
+    auth_key = AuthKey()
+    auth_key.set_content_change_callback(lambda: None)
+
+    datacenter_info = TelegramDatacenter.VESTA
+    address_resolver = CachedTransportAddressResolver()
+    transport_link_factory = TransportLinkTcpFactory(TransportCodecPerformanceTrackFactory(TransportCodecAbridgedFactory()), address_resolver)
+    blocking_executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+    crypto_provider = CryptoProviderCryptg()
+
+    session = Client(
+        datacenter_info,
+        auth_key,
+        connection_info,
+        transport_link_factory,
+        blocking_executor,
+        crypto_provider,
+        use_perfect_forward_secrecy=True,
+        no_updates=False,
+        error_description_resolver=None
+    )
+
+    configuration = await session.rpc_call({"_cons": "help.getConfig"})
+    address_resolver.apply_telegram_config(TelegramDatacenter.ALL_DATACENTERS, configuration)
+    session.disconnect()
+
+    while True:
+        try:
+            _ = await session.rpc_call({"_cons": "help.getConfig"})
+        except:
+            traceback.print_exc()
+        finally:
+            session.disconnect()
+
+
+if __name__ == "__main__" or __name__ == "uwsgi_file_test":
+    logging.getLogger().setLevel(level=logging.ERROR)
+    asyncio.run(test())
