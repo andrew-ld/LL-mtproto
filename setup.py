228681367b5aa7d67816311d469ee8817c7ed6a0
Andrea Cavalli <andrea@cavallium.it>
2024-06-28 14:50:29 +0200

    Update pyproject

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup
-
 from mypyc.build import mypycify
 
-setup(
-    ext_modules=mypycify([
-        '--strict',
-        '--pretty',
-        '--enable-incomplete-feature=NewGenericSyntax',
-        'll_mtproto/tl/tl.py'
-    ]),
-)
+setup(ext_modules=mypycify([]))
