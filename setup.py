a871222330fa99ac463a72107dea22955a73de90
Andrea Cavalli <andrea@cavallium.it>
2025-03-26 16:27:05 +0100

    Implement setup.py (#18)

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
+import pathlib
 from setuptools import setup
 from mypyc.build import mypycify
 
-setup(ext_modules=mypycify([]))
+here = pathlib.Path(__file__).parent
+init = here / "ll_mtproto" / "__init__.py"
+readme_path = here / "README.md"
+
+with open("requirements.txt", encoding="utf-8") as r:
+    requires = [i.strip() for i in r]
+
+with readme_path.open() as f:
+    README = f.read()
+
+setup(
+    name='ll_mtproto',
+    description='abstraction-free mtproto client',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/andrew-ld/LL-mtproto',
+    packages=["ll_mtproto", ],
+    ext_modules=mypycify([]),
+    python_requires='>=3.12.0',
+    install_requires=requires
+)
