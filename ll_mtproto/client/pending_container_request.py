3e43efa2fa0e443e71a4f97d5e478ba0166700f7
Your Name <you@example.com>
2025-05-18 10:03:37 +0200

    validate pending container request inner requests

--- ll_mtproto/client/pending_container_request.py
+++ ll_mtproto/client/pending_container_request.py
@@ -6,7 +6,17 @@ class PendingContainerRequest:
 
     requests: list[PendingRequest]
 
+    @staticmethod
+    def _validate_request(request: PendingRequest):
+        if not request.allow_container:
+            raise TypeError(f"Pending request `{request!r}` dont allow container.")
+
+        if not request.expect_answer:
+            raise TypeError(f"Pending request `{request!r}` dont expect an answer.")
+
     def __init__(self, requests: list[PendingRequest], last_message_id: int | None = None) -> None:
+        for request in requests:
+            self._validate_request(request)
         self.requests = requests
         self.last_message_id = last_message_id
 
