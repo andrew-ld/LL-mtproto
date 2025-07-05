0b60c1f1ba2fd903dc26019e8628905028bfee07
Your Name <you@example.com>
2025-07-05 20:54:09 +0200

    support clang

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ cpp_extension = Extension(
     'll_mtproto.crypto.providers.crypto_provider_openssl._impl',
     sources=['ll_mtproto/crypto/providers/crypto_provider_openssl/_impl.cpp'],
     libraries=['crypto'],
-    extra_compile_args=['-std=c++17', '-Wall', '-Wextra', '-Werror', '-fno-exceptions', '-Ofast', '-flto'],
+    extra_compile_args=['-std=c++17', '-Wall', '-Wextra', '-Werror', '-fno-exceptions', '-O3', '-flto'],
     extra_link_args=['-flto'],
     language='c++',
 )
